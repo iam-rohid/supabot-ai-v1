713711245101e5671ecc31e7949d31d6988508a0
Rohid <rohidul209@gmail.com>
2023-07-26 13:26:32 +0600

    Home Header

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,4 +1,5 @@
-import { APP_DOMAIN, APP_NAME } from "@/lib/constants";
+import { Button } from "@/components/ui/button";
+import { APP_DOMAIN, APP_NAME, HOME_DOMAIN } from "@/lib/constants";
 import { Metadata } from "next";
 import Link from "next/link";
 import React from "react";
@@ -14,11 +15,38 @@ export default function HomeLayout({
 }) {
   return (
     <div>
-      <Link href="/">Home</Link>
-      <Link href="/pricing">Pricing</Link>
-
-      <Link href={`${APP_DOMAIN}/login`}>Sign In</Link>
-      <Link href={`${APP_DOMAIN}/register`}>Sign Up</Link>
+      <header className="bg-card text-card-foreground border-b">
+        <div className="container h-16 flex items-center justify-between">
+          <Link
+            href={HOME_DOMAIN}
+            className="text-xl font-bold text-accent-foreground"
+          >
+            {APP_NAME}
+          </Link>
+          <div className="flex items-center">
+            <nav className="flex items-center gap-8 mr-8">
+              <Link
+                className="text-sm font-medium hover:text-accent-foreground text-muted-foreground"
+                href="/"
+              >
+                Home
+              </Link>
+              <Link
+                className="text-sm font-medium hover:text-accent-foreground text-muted-foreground"
+                href="/pricing"
+              >
+                Pricing
+              </Link>
+            </nav>
+            <Button variant="ghost" asChild className="mr-3">
+              <Link href={`${APP_DOMAIN}/login`}>Sign In</Link>
+            </Button>
+            <Button>
+              <Link href={`${APP_DOMAIN}/register`}>Sign Up</Link>
+            </Button>
+          </div>
+        </div>
+      </header>
       {children}
     </div>
   );
