22833923e7cf702303677c1898f5b4652bb77220
Rohid <rohidul209@gmail.com>
2023-07-27 16:59:02 +0600

    new zinc theme

--- src/app/app/(auth)/signin/page.tsx
+++ src/app/app/(auth)/signin/page.tsx
@@ -1,9 +1,7 @@
-import { APP_NAME, HOME_DOMAIN } from "@/lib/constants";
+import { APP_NAME } from "@/lib/constants";
 import { Metadata } from "next";
 import SignInForm from "./signin-form";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import { ChevronLeftIcon } from "lucide-react";
 import { Suspense } from "react";
 
 export const metadata: Metadata = {
@@ -13,19 +11,38 @@ export const metadata: Metadata = {
 export default function SignInPage() {
   return (
     <div className="relative flex min-h-screen flex-col justify-center py-16">
-      <div className="fixed left-0 right-0 top-0">
-        <div className="container flex h-16 items-center">
-          <Button variant="ghost" asChild>
-            <Link href={HOME_DOMAIN}>
-              <ChevronLeftIcon size={20} className="-ml-1 mr-2" />
-              Home
-            </Link>
-          </Button>
+      <div className="mx-auto grid w-full max-w-md gap-6 px-6">
+        <div className="grid gap-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Sign in to {APP_NAME}
+          </h1>
+          <p className="text-sm text-muted-foreground">
+            Enter your email below to sign in to your account
+          </p>
         </div>
+
+        <Suspense>
+          <SignInForm />
+        </Suspense>
+
+        <p className="px-8 text-center text-sm text-muted-foreground">
+          By clicking continue, you agree to our{" "}
+          <Link
+            href="/terms"
+            className="underline underline-offset-4 hover:text-primary"
+          >
+            Terms of Service
+          </Link>{" "}
+          and{" "}
+          <Link
+            href="/privacy"
+            className="underline underline-offset-4 hover:text-primary"
+          >
+            Privacy Policy
+          </Link>
+          .
+        </p>
       </div>
-      <Suspense fallback={<p>Loading...</p>}>
-        <SignInForm />
-      </Suspense>
     </div>
   );
 }
--- src/app/app/(auth)/signin/signin-form.tsx
+++ src/app/app/(auth)/signin/signin-form.tsx
@@ -14,7 +14,7 @@ import { Input } from "@/components/ui/input";
 import { useToast } from "@/components/ui/use-toast";
 import { APP_NAME } from "@/lib/constants";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Loader2Icon } from "lucide-react";
+import { ArrowRightIcon, Loader2Icon } from "lucide-react";
 import { signIn } from "next-auth/react";
 import { useSearchParams } from "next/navigation";
 import { useCallback, useEffect, useMemo, useState } from "react";
@@ -44,7 +44,6 @@ export default function SignInForm() {
     () => form.formState.isSubmitting || oauthSingInOpen,
     [form.formState.isSubmitting, oauthSingInOpen],
   );
-  const [isLoaded, setIsLoaded] = useState(false);
 
   const handleSubmit = useCallback(
     async ({ email }: SchemaData) => {
@@ -72,14 +71,9 @@ export default function SignInForm() {
   );
 
   return (
-    <div className="mx-auto w-full max-w-md p-6">
-      <div className="mb-6 h-12 w-12 rounded-full bg-primary" />
-      <h1 className="mb-8 text-2xl font-semibold">Sign in to {APP_NAME}</h1>
+    <div className="grid gap-4">
       <Form {...form}>
-        <form
-          className="flex flex-col gap-6"
-          onSubmit={form.handleSubmit(handleSubmit)}
-        >
+        <form className="grid gap-4" onSubmit={form.handleSubmit(handleSubmit)}>
           <FormField
             control={form.control}
             name="email"
@@ -99,40 +93,42 @@ export default function SignInForm() {
           />
 
           <Button disabled={isLoading} type="submit" className="w-full">
-            {isLoading && (
-              <Loader2Icon size={20} className="mr-2 animate-spin" />
+            {form.formState.isSubmitting ? (
+              <Loader2Icon className="h-4 w-4 animate-spin" />
+            ) : (
+              <>
+                Continue with Email
+                <ArrowRightIcon className="ml-2 h-4 w-4" />
+              </>
             )}
-            Continue with Email
           </Button>
         </form>
       </Form>
 
-      <div className="my-6 flex items-center gap-4">
+      <div className="flex items-center gap-4">
         <div className="h-px flex-1 bg-border" />
         <p className="text-sm text-muted-foreground">OR</p>
         <div className="h-px flex-1 bg-border" />
       </div>
 
-      <div className="flex flex-col gap-3">
-        <Button
-          variant="outline"
-          className="w-full"
-          onClick={() => {
-            setOauthSingInOpen(true);
-            signIn("github", {
-              ...(callbackUrl && callbackUrl.length > 0 ? { callbackUrl } : {}),
-            });
-          }}
-          disabled={isLoading}
-        >
-          {isLoading ? (
-            <Loader2Icon size={20} className="mr-2 animate-spin" />
-          ) : (
-            <GithubIcon className="mr-2 text-2xl" />
-          )}
-          Sign in with Github
-        </Button>
-      </div>
+      <Button
+        variant="outline"
+        className="w-full"
+        onClick={() => {
+          setOauthSingInOpen(true);
+          signIn("github", {
+            ...(callbackUrl && callbackUrl.length > 0 ? { callbackUrl } : {}),
+          });
+        }}
+        disabled={isLoading}
+      >
+        {oauthSingInOpen ? (
+          <Loader2Icon className="mr-2 h-4 w-4 animate-spin" />
+        ) : (
+          <GithubIcon className="mr-2 text-xl" />
+        )}
+        Sign in with Github
+      </Button>
     </div>
   );
 }
